9bf41eb5b84c1adc990c9e0f238b02cb3594918e
calindotgabriel <calindotgabriel@yahoo.com>
2014-05-29 19:03:41 +0300

    added drawer, need to work on colors

---
+++ src/csoft/ro/transsist/Item.java
@@ -0,0 +1,26 @@
+package csoft.ro.transsist;
+
+/**
+ * @author motan
+ * @date 5/29/14
+ */
+public class Item {
+
+
+    private String name;
+
+
+    public Item(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+}
--- src/csoft/ro/transsist/MainActivity.java
+++ src/csoft/ro/transsist/MainActivity.java
@@ -1,15 +1,112 @@
 package csoft.ro.transsist;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.widget.DrawerLayout;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.app.SherlockActivity;
+
+import java.util.ArrayList;
 
 public class MainActivity extends SherlockActivity {
     /**
-     * Called when the activity is first created.
+     *
      */
+    ActionBarDrawerToggle mDrawerToggle;
+    DrawerLayout mDrawerLayout;
+    ListView mDrawerList;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        loadDrawer();
+    }
+
+    public void loadDrawer () {
+
+        mDrawerLayout =
+                (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList =
+                (ListView) findViewById(R.id.list);
+        mDrawerList.setOnItemClickListener(new SlideMenuClickListener());
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setHomeButtonEnabled(true);
+
+        ArrayList<Item> drawerItems = new ArrayList<Item>();
+        drawerItems.add(new Item("Program"));
+        drawerItems.add(new Item("Harta"));
+
+        DrawerItemAdapter adapter =
+                new DrawerItemAdapter(this, drawerItems);
+        mDrawerList.setAdapter(adapter);
+
+        mDrawerToggle =
+                new ActionBarDrawerToggle(this, mDrawerLayout,
+                R.drawable.ic_drawer, R.string.app_name, R.string. app_name) {
+                    @Override
+                    public void onDrawerOpened(View drawerView) {
+                        super.onDrawerOpened(drawerView);
+                    }
+
+                    @Override
+                    public void onDrawerClosed(View drawerView) {
+                        super.onDrawerClosed(drawerView);
+                    }
+                };
+
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+
+    }
+
+    private class SlideMenuClickListener implements
+            ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position,
+                                long id) {
+            displayView(position);
+        }
+    }
+
+    public void displayView(int position) {
+        Toast.makeText(this, "Clicked " + position, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onMenuItemSelected(int featureId, com.actionbarsherlock.view.MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                if (mDrawerLayout.isDrawerOpen(mDrawerList)) {
+                    mDrawerLayout.closeDrawer(mDrawerList);
+                } else {
+                    mDrawerLayout.openDrawer(mDrawerList);
+                }
+                return true;
+
+        }
+        return true;
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        // Pass any configuration change to the drawer toggls
+        mDrawerToggle.onConfigurationChanged(newConfig);
     }
 }
