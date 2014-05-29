8ea5da0e4b1b2951a2017dd75d96a8a07acd88eb
calindotgabriel <calindotgabriel@yahoo.com>
2014-05-29 18:15:24 +0300

    Initial commit

---
+++ src/csoft/ro/transsist/MainActivity.java
@@ -0,0 +1,15 @@
+package csoft.ro.transsist;
+
+import android.os.Bundle;
+import com.actionbarsherlock.app.SherlockActivity;
+
+public class MainActivity extends SherlockActivity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
